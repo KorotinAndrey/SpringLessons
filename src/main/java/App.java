21ed4bb320b717f484c3b76ded6bcdf771a643c5
xpc <korotinandrey@mail.ru>
2023-09-05 20:16:55 +0400

    SpringLessons

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,19 @@
-import org.springframework.context.ApplicationContext;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld hello1 = context.getBean("helloWorld", HelloWorld.class);
+        HelloWorld hello2 = context.getBean("helloWorld", HelloWorld.class);
+
+        Cat cat1 = context.getBean("cat", Cat.class);
+        Cat cat2 = context.getBean("cat", Cat.class);
+
+        System.out.println("Сравнение HelloWorld: " + (hello1 == hello2));
+        System.out.println("Сравнение Cat: " + (cat1 == cat2));
+
+        context.close();
     }
 }
